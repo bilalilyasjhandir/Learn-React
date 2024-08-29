c26b31034934fd28d0f3a33420ea3833dddfdb58
Bilal Ilyas Jhandir <bilaljhandir007@gmail.com>
2024-08-30 00:24:23 +0500

    custom react

--- vite_react/src/App.jsx
+++ vite_react/src/App.jsx
@@ -1,6 +1,7 @@
 import HelloWorld from "./HelloWorld"
 
 function App() {
+  const user = "This is a variable"
   return (
     // vite react returns only one element
     //so we use fragments to manipulate it
@@ -8,6 +9,7 @@ function App() {
       <HelloWorld/>
       <h1>Bilal</h1>
       <p>React.js is fun</p>
+      <p>{user}</p>
     </>
   )
 }
--- vite_react/src/main.jsx
+++ vite_react/src/main.jsx
@@ -1,9 +1,43 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {jsx as _jsx} from 'react/jsx-runtime.js'
+
+function MyApp(){
+    return(
+        <div>
+            <h1>This is a custom app</h1>
+        </div>
+    )
+}
+
+const reactElement = {
+    type: 'a',
+    props:{
+        href: 'https://www.linkedin.com/in/bilalilyasjhandir',
+        target: '_blank'
+    },
+    children: 'Click here to visit my LinkedIn'
+}
+
+const anotherElement = (
+    <a href="https://www.linkedin.com/in/bilalilyasjhandir" target='_blank'>Visit my LinkedIn</a>
+)
+
+    const anotherReactElement = React.createElement(
+    'a',
+    {href: 'https://www.linkedin.com/in/bilalilyasjhandir', target:'_blank'},
+    'Visit my LinkedIn'
+)
 
 createRoot(document.getElementById('root')).render(
   
     <App />
-  
+    // <MyApp />
+    //MyApp() //this will also work but do not write
+    // reactElement
+    // anotherElement
+    // anotherReactElement
 )
